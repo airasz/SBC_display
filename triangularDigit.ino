a70907aa8c40978981327a6c842e322345760b76
airasz <rirulez.mz@gmail.com>
2025-02-20 11:53:46 +0700

    working good add flicker animation

--- triangularDigit.ino
+++ triangularDigit.ino
@@ -1,19 +1,7 @@
 int tenSegment[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 // Segment activation patterns for numbers 0-9 (A-G segments)
-const byte tdigitPatterns[11] = {
-    0b1110110111, // 0
-    0b0011100011, // 1
-    0b0111111110, // 2
-    0b0011111111, // 3
-    0b1001101011, // 4
-    0b1011111101, // 5---
-    0b1111111101, // 6
-    0b0011110011, // 7
-    0b1111111111, // 8---
-    0b1011111111, // 9
-    0b0001001000, // 10 as "-" sign
-};
-uint32_t TenSegmenPaternts[11][10] = {
+
+uint32_t TenSegmenPaterns[11][10] = {
     {1, 1, 1, 0, 1, 1, 0, 1, 1, 1}, // 0
     {0, 0, 1, 1, 1, 0, 0, 0, 1, 1}, // 1
     {0, 1, 1, 1, 1, 1, 1, 1, 1, 0}, // 2
@@ -25,17 +13,6 @@ uint32_t TenSegmenPaternts[11][10] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 8
     {1, 0, 1, 1, 1, 1, 1, 1, 1, 1}, // 9
     {0, 0, 0, 1, 0, 0, 1, 0, 0, 0}  // 10 as "-" sign
-};
-
-// Segment positions (relative to x,y)
-const int tsegments[7][4] = {
-    {1, 0, 3, 0}, // A (top)
-    {4, 1, 4, 3}, // B (upper right)
-    {4, 4, 4, 6}, // C (lower right)
-    {1, 7, 3, 7}, // D (bottom)
-    {0, 4, 0, 6}, // E (lower left)
-    {0, 1, 0, 3}, // F (upper left)
-    {1, 4, 3, 4}  // G (middle)
 };
 
 uint32_t triangularpos[10][3][2] = {
@@ -162,43 +139,38 @@ void fillingTriangular(int x, int y, int size, int gap)
 void drawTSegment(int x, int y, int segment, bool state, int size, uint32_t col)
 {
     int x0, y0, x1, y1, x2, y2;
-
     x0 = (x + triangularpos[segment][0][0]);
     y0 = (y + triangularpos[segment][0][1]);
     x1 = (x + triangularpos[segment][1][0]);
     y1 = (y + triangularpos[segment][1][1]);
     x2 = (x + triangularpos[segment][2][0]);
     y2 = (y + triangularpos[segment][2][1]);
-
-    if (state)
-    {
-        tft.fillTriangle(x0, y0, x1, y1, x2, y2, col);
-    }
-    else
-    {
-        tft.drawTriangle(x0, y0, x1, y1, x2, y2, SS_DISABLE);
-    }
+    tft.fillTriangle(x0, y0, x1, y1, x2, y2, (state) ? col : SS_DISABLE);
 }
 
 void drawT7Segment(int x, int y, int number, int size, uint32_t col)
 {
     fillingTriangular(x, y, size, 4);
-    byte pattern = tdigitPatterns[number];
-    // for (int i = 0; i < 7; i++)
-    // {
-    //     // bool state = bitRead(pattern, 6 - i); // Bits are A to G (MSB to LSB)
-    //     bool state = bitRead(pattern, 6 - sevensegment[i]); // Bits are A to G (MSB to LSB)
-    //     drawSegment(x, y, sevensegment[i], true, size, SS_DISABLE);
-    //     delay(50);
-    // }
-    Serial.print("Pattern: ");
-    Serial.println(pattern, BIN);
+    uint16_t tmppaterns[10];
     for (int i = 0; i < 10; i++)
     {
-
-        // bool state = bitRead(pattern, 9 - tenSegment[i]); // Bits are A to G (MSB to LSB)
-        bool state = TenSegmenPaternts[number][i];
-        Serial.print(state);
+        tmppaterns[i] = TenSegmenPaterns[number][i];
+    }
+    for (int i = 0; i < 10; i++) // random flicker animation before displaying the number
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            bool state = random(2);
+            // Serial.print(state);
+            drawTSegment(x, y, tenSegment[i], state, size, col);
+            delay(5);
+        }
+        delay(25);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        bool state = TenSegmenPaterns[number][i];
+        // Serial.print(state);
         drawTSegment(x, y, tenSegment[i], state, size, col);
         delay(75);
     }
@@ -206,15 +178,21 @@ void drawT7Segment(int x, int y, int number, int size, uint32_t col)
 // String old_score;
 void tsgmnt(String score)
 {
+    String scores = data.substring(data.indexOf(">"));
+    String homescore = scores.substring(scores.indexOf(">") + 2, scores.indexOf("-"));
+    String awayscore = scores.substring(scores.indexOf("-") + 1);
+    String hometeam = data.substring(0, data.indexOf("vs"));
+    String awayteam = data.substring(data.indexOf("vs") + 2, data.indexOf(">"));
+    String matchtime = awayteam.substring(data.indexOf("\n"));
+
+    int number = homescore.toInt();
+    int ascr = awayscore.toInt();
+    int sz = 16; // segment size
+    int x = 1, y = 20;
+    int nextpos = (sz * 2) - (sz / 2);
+
     if (score != old_score)
     {
-        String scores = data.substring(data.indexOf(">"));
-        String homescore = scores.substring(scores.indexOf(">") + 2, scores.indexOf("-"));
-        String awayscore = scores.substring(scores.indexOf("-") + 1);
-        String hometeam = data.substring(0, data.indexOf("vs"));
-        String awayteam = data.substring(data.indexOf("vs") + 2, data.indexOf(">"));
-        String matchtime = awayteam.substring(data.indexOf("\n"));
-
         tft.fillScreen(TFT_BLACK);
         cx = 0, cy = 0;
         drawtext(hometeam, COLOR_MEDIUM[random(10)]);
@@ -222,59 +200,25 @@ void tsgmnt(String score)
         // tft.print(awayteam);
         cx = 0, cy = 112;
         drawtext(awayteam, COLOR_MEDIUM[random(10)]);
-        int number = homescore.toInt();
-        int ascr = awayscore.toInt();
-        int sz = 10; // segment size
-        int x = 10, y = 30;
         int sssize = sizeof(sevensegment) / sizeof(sevensegment[0]);
-        // shuffleArray(sevensegment, sssize);
-        drawT7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // draw7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 5), y, 10, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 10), y, ascr, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
+        drawT7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]);                   // Position at (20,20) with size 10
+        drawT7Segment(x + nextpos, y, 10, sz, COLOR_MEDIUM[random(12)]);             // Position at (20,20) with size 10
+        drawT7Segment(x + nextpos + nextpos, y, ascr, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
         old_score = score;
     }
     else
     {
-        String scores = data.substring(data.indexOf(">"));
-        String homescore = scores.substring(scores.indexOf(">") + 2, scores.indexOf("-"));
-        String awayscore = scores.substring(scores.indexOf("-") + 1);
-        String hometeam = data.substring(0, data.indexOf("vs"));
-        String awayteam = data.substring(data.indexOf("vs") + 2, data.indexOf(">"));
-
         // tft.fillScreen(TFT_BLACK);
         cx = 0, cy = 0;
         drawtext(hometeam, TFT_BLACK);
         drawtext(hometeam, COLOR_MEDIUM[random(10)]);
-        // tft.setCursor(0, 82);
-        // tft.print(awayteam);
         cx = 0, cy = 112;
         drawtext(awayteam, TFT_BLACK);
         drawtext(awayteam, COLOR_MEDIUM[random(10)]);
-        int number = homescore.toInt();
-        int ascr = awayscore.toInt();
-        int sz = 10; // segment size
-        int x = 10, y = 30;
-
         int sssize = sizeof(sevensegment) / sizeof(sevensegment[0]);
-
-        drawT7Segment(x, y, number, sz, TFT_BLACK);                // Position at (20,20) with size 10
-        drawT7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // draw7Segment(x, y, 8, sz, TFT_BLACK); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 5), y, 8, sz, TFT_BLACK); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 10), y, 8, sz, TFT_BLACK); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 5), y, 10, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-        // shuffleArray(sevensegment, sssize);
-        // draw7Segment(x + (sz * 10), y, ascr, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
-
-        // drawDigit(60, 10, 10, number, COLOR_MEDIUM[random(12)]);
+        drawT7Segment(x, y, number, sz, COLOR_MEDIUM[random(12)]);                   // Position at (20,20) with size 10
+        drawT7Segment(x + nextpos, y, 10, sz, COLOR_MEDIUM[random(12)]);             // Position at (20,20) with size 10
+        drawT7Segment(x + nextpos + nextpos, y, ascr, sz, COLOR_MEDIUM[random(12)]); // Position at (20,20) with size 10
     }
 }
 void testDTSegment(int number)
